e440f47ee77898bec997112f1d2f078ad2f7be26
Kelvin Li <ltwisabc@gmail.com>
2013-01-11 18:15:42 -0800

    Add cluster and annealment logic
    completely untested

--- dna.py
+++ dna.py
@@ -23,15 +23,67 @@ def _min_rotation(sequence):
     sequence under lexicographical order. The `n'-step left-rotation is:
         sequence[n:] + sequence[:n]
     """
-    size = len(sequence) # constant
+    L = len(sequence)
     depth = 0
-    candidates = tuple(range(size))
-    while depth < size and len(candidates) > 1:
-        vals = tuple(sequence[(c + depth) % size] for c in candidates)
+    candidates = tuple(range(L))
+    while depth < L and len(candidates) > 1:
+        vals = tuple(sequence[(c + depth) % L] for c in candidates)
         lightest = min(vals)
-        candidates = tuple(c for c, v in zip(candidates, vals) if v == lightest)
+        candidates = tuple(c for c, v in zip(candidates, vals)
+                           if v == lightest)
         depth += 1
     return candidates[0]
+
+def _circular_fragment(sequence, start, length):
+    """
+    Returns the subsequence of given length, beginning at index `start',
+    treating `sequence' as circular.
+    """
+    L = len(sequence)
+    if not L or length < 0 or length > L:
+        raise IndexError
+    start %= L
+    end = (start + length) % L
+    if start < end:
+        return sequence[start:end]
+    return sequence[start:] + sequence[:end]
+
+def _has_overlap(starts_ends):
+    """
+    Given an iterable of the form [(start0, end0), (start1, end1), ...],
+    returns whether the half-open intervals have any overlap.
+    """
+    taken = list()
+    for s, e in starts_ends:
+        if not s < e:
+            raise ValueError
+        if any(not (old_e <= s or e <= old_s) for old_s, old_e in taken):
+            return True
+        taken.append((s, e))
+    return False
+
+def _normalized_circular_starts_ends(starts_ends, L):
+    """
+    Normalizes the input to `_has_overlap', assuming `starts_ends'
+    specifies intervals on a circular sequence of size `L'.
+    """
+    iter = ((s % L, e % L) for s, e in starts_ends)
+    for s, e in iter:
+        if s < e:
+            yield (s, e)
+            continue
+        yield (s, L)
+        if e:
+            yield (0, e)
+
+def _sequence_has_overlap(sequence, starts_ends):
+    """
+    Returns True if `starts_ends' half-open intervals overlap on `sequence'.
+    """
+    return _has_overlap(_normalized_circular_starts_ends(starts_ends,
+                                                         len(sequence))
+                        if sequence.is_circular()
+                        else starts_ends)
 
 class Nucleotide:
     def __init__(self, wildcard):
@@ -55,10 +107,6 @@ class Nucleotide:
     def is_complement(self, other):
         return other._wildcard == \
                frozenset(_nucleotides[base] for base in self._wildcard)
-
-    def complement(self):
-        return Nucleotide(_nucleotides[base]
-                          for base in self._wildcard)
 
     def sort_key(self):
         return tuple(2 ** i
@@ -97,6 +145,13 @@ class LinearSequence(BaseSequence):
     def is_circular(self):
         return False
 
+    # TODO: transfer properties?
+    def fragment(self, start, length):
+        if length < 0:
+            raise IndexError
+        end = start + length
+        return LinearSequence(self._nucleotides[start:end])
+
 class CircularSequence(BaseSequence):
     def __init__(self, nucleotides):
         amount = _min_rotation(n.sort_key() for n in nucleotides)
@@ -109,3 +164,68 @@ class CircularSequence(BaseSequence):
 
     def is_circular(self):
         return True
+
+    # TODO: transfer properties?
+    def fragment(self, start, length):
+        frag = _circular_fragment(self._nucleotides, start, length)
+        return LinearSequence(frag)
+
+class _Annealment:
+    def __init__(self, sequences, starts, length):
+        self.sequences = tuple(sequences)
+        self.starts = tuple(starts)
+        self._length = length
+        self._validate()
+
+    def _validate(self):
+        f0, f1 = tuple(seq.fragment(start, self._length)
+                       for seq, start in zip(self.sequences, self.starts))
+        if not f0.is_reverse_complement(f1):
+            raise ValueError("sequences must be reverse complementary over " \
+                             "annealment region")
+
+    def ends(self):
+        return tuple(s + self._length for s in self.starts)
+
+    def has_overlap(self, other):
+        # TODO: there is room for optimization
+        for s, o in (0, 0), (0, 1), (1, 0), (1, 1):
+            common_sequence = self.sequences[s]
+            if common_sequence is not other.sequences[o]:
+                continue
+            common_starts_ends = ((self.starts[s], self.ends()[s]),
+                                  (other.starts[o], other.ends()[o]))
+            if _sequence_has_overlap(common_sequence, common_starts_ends):
+                return True
+        return False
+
+class Cluster:
+    def __init__(self):
+        self._sequences = list()
+        self._annealments = list()
+
+    def add_sequence(self, new):
+        if new in self._sequences:
+            raise ValueError("cluster already contains this sequence")
+        self._sequences.append(new)
+
+    def _validate_pre_add_annealment(self, sequences, starts, length):
+        if not len(sequences) == len(starts) == 2:
+            raise ValueError("must have two sequences and two start points")
+        if not length > 0:
+            raise ValueError("length must be positive")
+        if not all(s >= 0 for s in starts):
+            raise ValueError("start points must be non-negative")
+        if not all(s in self._sequences for s in sequences):
+            raise ValueError("cluster must contain the input sequences")
+
+    def add_annealment(self, sequences, starts, length, *, overwrite=False):
+        sequences = tuple(sequences)
+        starts = tuple(starts)
+        _validate_pre_add_annealment(sequences, starts, length)
+        new = _Annealment(sequences, starts, length)
+        if overwrite:
+            raise NotImplementedError
+        if any(new.has_overlap(old) for old in self._annealments):
+            raise ValueError("refusing to overwrite existing annealment")
+        self._annealments.append(new)
